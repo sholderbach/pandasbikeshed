1f3ba6209d6272deb4b26dc18cddfcf087db22a9
sholderbach <ho.steve@web.de>
2019-09-02 22:50:20 +0200

    Small tool to convert clipboard tables to tex

---
+++ pandasbikeshed/cli_tools/clip2tex.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import argparse
+
+def run(filename=None, escape=False, print_it=True):
+    try:
+        df = pd.read_clipboard(sep='\t')
+    except:
+        print('Could not read from clipboard. Make sure your clipboard contains tab separated data!')
+        return 1
+    df = df.replace(pd.np.nan, '')
+    df_str = df.to_latex(escape=escape, index=False)
+    if filename:
+        with open(filename, 'w') as f:
+            f.write(df_str)
+    if print_it:
+        print(df_str)
+    return 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='clip2tex', description='A small command-line tool to convert tables copied to the clipboard e.g. from MS Excel or GDrive to a simple LaTeX')
+    parser.add_argument('FILENAME', required=False,)
+    parser.add_argument('--escape', '-e', action='store_true', help='Set to escape all special characters')
+    args = parser.parse_args()
+    if args.FILENAME:
+        run(filename=args.FILENAME, escape=args.escape, print_it=False)
+    else:
+        run(escape=args.escape)
+
